afe0377fe51f03a2b763bfa097c245881ac8686a
github username <github email address>
2019-06-10 17:15:20 +0100

    Completed task

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,37 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    # Returns an array of all the galleries
+    @@all
+  end
+
+  def paintings
+    # Returns an array of all paintings in a gallery
+    Painting.all.select{|p| p.gallery == self}
+  end
+
+  def artists
+  # Returns an array of all artists that have a painting in a gallery
+    paintings.collect{|p| p.artist}.uniq
+  end
+
+  def artist_names
+    # Returns an array of the names of all artists that have a painting in a gallery
+    artists.collect{|a| a.name}
+  end
+
+  def most_expensive_painting
+    # Returns an instance of the most expensive painting in a gallery
+    paintings.max_by{|p| p.price}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,15 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Bob Ross", 20)
+a2 = Artist.new("Picasso", 30)
+a3 = Artist.new("Matisse", 10)
+
+g1 = Gallery.new("Louvre", "Paris")
+g2 = Gallery.new("Tate", "London")
 
 
+
+a1.create_painting("the blob", 5000, g1)
 
 binding.pry
 
