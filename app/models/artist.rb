2f5e01552a3367f0f0ebc4613f7db82d8122d562
Louise Norris <louisenorris73@yahoo.co.uk>
2019-06-10 19:35:46 +0100

    completed practice lab

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,44 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting| painting.artist == self }
+  end
+
+  def galleries
+    paintings.map{|painting| painting.gallery }
+  end
+
+  def cities
+    galleries.map{|painting| painting.city }
+  end
+
+  def self.total_experience
+    Artist.all.sum(&:years_experience).to_i
+  end
+
+  def prolific
+    (paintings.count.to_f / self.years_experience)
+  end
+
+  def self.most_prolific
+    Artist.all.max_by{|artist| artist.prolific }
+  end
+
+  def self.create_painting(title, price, gallery)
+    Painting.new(self, title, price, gallery)
   end
 
 end
