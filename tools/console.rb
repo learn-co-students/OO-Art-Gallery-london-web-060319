7c64c51241722a009a32203d484ba6a0d4a481b9
Le H Ngo <seiji@o2.pl>
2019-06-10 19:34:07 +0100

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,16 @@
 require_relative '../config/environment.rb'
 
+le = Artist.new("Le H", 10)
+annie = Artist.new("Anniemaria", 6)
+aruna = Artist.new("Aruna", 18)
 
+vatican = Gallery.new("Vaticanos", "Italy")
+france = Gallery.new("Paris Art Gallery", "France")
+poland = Gallery.new("Zonk Art", "Poland")
+
+p1_picasso = Painting.new("Beauty", 2000, le, poland)
+p2_amazon = Painting.new("Amazonia", 1200, le, france)
+p3_himalaya = Painting.new("Himalaya", 3000, annie, france)
 
 
 binding.pry
